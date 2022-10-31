886a5c2add28cf50ee082956a857967a47dc9041
unknown <bakytbekovabermett@gmail.com>
2022-10-31 17:37:09 +0300

    Initial commit

---
+++ storage/framework/views/af8eaf97852d51999e53f3aec9ec675077cce7f7.php
@@ -0,0 +1,7 @@
+<div style="clear:both" class="panel panel-default">
+    <div class="panel-body">
+      Panel content
+    </div>
+    <div class="panel-footer">Panel footer</div>
+  </div>
+  <?php /**PATH C:\Users\User\multisystem-en\resources\views/footer.blade.php ENDPATH**/ ?>
